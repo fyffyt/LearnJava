857ada7a57ca8f2ac78afbb1a5f44cfead49e5b8
jolyon <jolyon@jolyon-PC>
2015-05-09 00:17:21 +0800

    Init LearnJava Project

---
+++ src/LearnJava.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author jolyon
+ *
+ */
+public class LearnJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
