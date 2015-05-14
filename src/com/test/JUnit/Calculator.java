2f28e5252f8c8fd171f3a9407e8611349aa2d3b7
Yifei Fu <yifei.fu@baysensors.com>
2015-05-14 13:01:38 +0800

    add JUnit

---
+++ src/com/test/JUnit/Calculator.java
@@ -0,0 +1,40 @@
+package com.test.JUnit;
+import static org.junit.Assert.assertEquals;
+
+public class Calculator {
+    private static int result = 1; // 静态变量，用于存储运行结果
+    public void add(int n) {
+        result = result + n;
+    }
+    public void substract(int n) {
+        result = result - 1;  //Bug: 正确的应该是 result =result-n
+    }
+    public void multiply(int n) {
+    }         // 此方法尚未写好
+    public void divide(int n) {
+        result = result / n;
+    }
+    public void square(int n) {
+        result = n * n;
+    }
+    public void squareRoot(int n) {
+        for (; ;) ;            //Bug : 死循环
+    }
+    public void clear() {     // 将结果清零
+        result = 0;
+    }
+    public int getResult() {
+        return result;
+    }
+    static void test(){
+    	assertEquals(1,2);
+    } 
+    public static void main(String[] args){
+    	System.out.println("hello.\n");
+    	try{
+    		assert(false);
+    	}catch(Exception e){
+    		System.out.println(e.getMessage());
+    	}
+    }
+}
---
+++ src/com/test/JUnit/CalculatorParaTest.java
@@ -0,0 +1,64 @@
+package com.test.JUnit;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Collection;
+import java.util.Arrays;
+
+@RunWith(Parameterized.class)
+public class CalculatorParaTest {
+	private static Calculator calc;
+	private int param;
+	private int result;
+	public int tee;
+	
+	@BeforeClass
+	public static void InitData(){
+		calc = new Calculator();
+		System.out.println("tag1:"+calc.getResult());
+	}
+	
+	@Before
+	public void setUp(){
+		calc.clear();
+	}
+	
+	@Parameters
+	public static Collection<Object[]> data(){
+		return Arrays.asList(new Object [][] {
+				{2, 4},
+				{3, 9},
+				{0, 0},
+				{1, 1},
+				{4, 16},
+				{-5, 25}
+		});
+	}
+	
+	public CalculatorParaTest(int param, int result){
+		this.param = param;
+		this.result = result;
+		System.out.println("tag2:"+calc.getResult());
+	}
+	
+	@Test
+	public void inspector(){
+		calc.square(param);
+		assertEquals(result,calc.getResult());
+	}
+	
+	@Test
+	public void inspector2(){
+		calc.add(param);
+		assertEquals(result,calc.getResult());
+	}
+}
