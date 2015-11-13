93d8c93b74265d336ddf50dbd24ea6b4e8cb1ac1
Yifei Fu <yifei.fu@baysensors.com>
2015-11-13 12:24:17 +0800

    update

--- src/com/practise/awt/PractiseAWT.java
+++ src/com/practise/awt/PractiseAWT.java
@@ -2,6 +2,7 @@ package com.practise.awt;
 
 import java.awt.*;
 import java.nio.CharBuffer;
+import java.io.*;
 
 public class PractiseAWT {
 
@@ -10,7 +11,17 @@ public class PractiseAWT {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubf
-
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(System.in));
+		String ln;
+		try {
+			while((ln = in.readLine())!= null){
+				System.out.println(ln);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/com/practise/socket/PractiseSocket.java
+++ src/com/practise/socket/PractiseSocket.java
@@ -39,6 +39,45 @@ class Server extends Thread{
 	
 }
 
+class Client extends Thread{
+	private Socket echoSocket = null;
+	private final String hostname = "127.0.0.1";
+	private final static int svrPort = 9946;
+	
+	public void run(){
+		try {
+			echoSocket = new Socket(hostname, svrPort);
+
+			BufferedReader in = new BufferedReader(
+						new InputStreamReader(
+								echoSocket.getInputStream()
+						)
+					);
+			PrintWriter out = new PrintWriter(
+					
+					echoSocket.getOutputStream()
+							
+					);
+			BufferedReader stdIn = new BufferedReader(
+					new InputStreamReader(
+							System.in
+					)
+				);
+			String inputLine;
+			while((inputLine = stdIn.readLine()) != null){
+				System.out.println("tag1");
+				out.println(inputLine);
+				System.out.println("echo: " + in.readLine());
+			}
+			System.out.println("terminate client");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
+	
+}
+
 public class PractiseSocket {
 
 	/**
@@ -47,7 +86,9 @@ public class PractiseSocket {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Server svr = new Server();
+		Client client = new Client();
 		svr.start();
+		client.start();
 	}
 
 }
